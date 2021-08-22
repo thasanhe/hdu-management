e680e534b50af81983e92c97bd0981461ba3ff4c
Tharindu Hewawitharana <thasanhe1990@gmail.com>
2021-08-23 00:22:45 +0530

    Adding search bar

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -10,12 +10,72 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final searchController = TextEditingController();
+  bool isSearchTapped = false;
+  bool isTyping = false;
+
+  onSearchTap() {
+    setState(() {
+      this.isSearchTapped = true;
+      print('search tapped');
+    });
+  }
+
+  clearSearch() {
+    setState(() {
+      print('tapped');
+      this.isSearchTapped = false;
+      this.isTyping = false;
+      searchController.clear();
+      print(isSearchTapped);
+    });
+  }
+
+  onChanged(text) {
+    setState(() {
+      this.isTyping = searchController.text.isNotEmpty;
+      print(isTyping);
+    });
+  }
+
+  handleSearch(String query) {}
+
+  AppBar buildSearchField() {
+    return AppBar(
+      title: TextFormField(
+        onTap: onSearchTap,
+        onChanged: onChanged,
+        style: TextStyle(color: Colors.white),
+        controller: searchController,
+        decoration: InputDecoration(
+          hintText: 'Search patient',
+          filled: true,
+          fillColor: Colors.blue.shade400,
+          suffixIcon: isTyping
+              ? IconButton(
+                  icon: Icon(Icons.clear),
+                  color: Colors.white,
+                  onPressed: clearSearch,
+                )
+              : IconButton(
+                  icon: Icon(Icons.search),
+                  color: Colors.white,
+                  onPressed: () {},
+                ),
+          hintStyle: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        onFieldSubmitted: handleSearch,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Overview"),
-        ),
-        drawer: AppDrawer());
+      appBar: buildSearchField(),
+      drawer: AppDrawer(),
+    );
   }
 }
